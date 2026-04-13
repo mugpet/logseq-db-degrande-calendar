7ec798e2f95b56b4a62e2e811675f07bbaea1560
Mugo Petersen <mugpo.petersen@fellowmind.dk>
2026-04-13 10:59:27 +0200

    Refresh release screenshots

--- plugin.js
+++ plugin.js
@@ -1,5 +1,5 @@
 const STARTUP_ERROR_PREFIX = "[Degrande Calendar] Failed to start";
-const FALLBACK_PLUGIN_VERSION = "0.1.3";
+const FALLBACK_PLUGIN_VERSION = "0.1.4";
 const MAIN_SCRIPT_DATA_ATTRIBUTE = "data-degrande-calendar-main";
 
 let pluginStartupPromise = null;
